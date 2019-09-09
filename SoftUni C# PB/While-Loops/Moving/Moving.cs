f8c8d170173d34ee5e6aa6ca1152035579f9a186
DESKTOP-46GR877\mayae <maya.ema@gmail.com>
2019-09-09 16:22:24 +0300

    add homework lab exercise

---
+++ SoftUni C# PB/While-Loops/Moving/Moving.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Moving
+{
+    class Moving
+    {
+        static void Main(string[] args)
+        {
+            var roomWidth = int.Parse(Console.ReadLine());
+            var roomLength = int.Parse(Console.ReadLine());
+            var roomHight = int.Parse(Console.ReadLine());
+            var roomFreeSpace = roomHight * roomLength * roomWidth;
+            var done = true;
+            var spaceNeeded = 0;
+            var boxes = 0;
+            while (done)
+            {
+                var command = Console.ReadLine();
+                if (command == "Done")
+                {
+                    done = false;
+                    Console.WriteLine($"{roomFreeSpace} Cubic meters left.");
+                    break;
+                }
+                roomFreeSpace -= int.Parse(command);
+                if (roomFreeSpace < 0)
+                {
+                    spaceNeeded = roomFreeSpace * -1;
+                    Console.WriteLine($"No more free space! You need {spaceNeeded} Cubic meters more.");
+                    break;
+                }
+            }
+        }
+    }
+}
